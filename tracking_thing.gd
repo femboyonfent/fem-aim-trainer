7cb5aa4ca8a4ab5a1da080fcbeec2c0ac5158c43
real <144968053+femboyonfent@users.noreply.github.com>
2024-10-23 15:29:25 -0400

    QJHB<jeafh,JHESF
    fe!n

--- tracking_thing.gd
+++ tracking_thing.gd
@@ -24,7 +24,7 @@ func _input(event):
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_speed))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89),deg_to_rad( 89))
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_pressed("attack"):
 		accuracey = hits / attempts
 		attempts = attempts + 1 
 		_shoot_USPS()
@@ -32,17 +32,6 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	
-	var input_dir := Input.get_vector("left", "right", "forward ", "backword")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
 	move_and_slide()
 
 func _shoot_USPS():
@@ -50,11 +39,8 @@ func _shoot_USPS():
 		$head/Camera3D/usps/AnimationPlayer.play("shoot")
 		if aim_ray.is_colliding():
 			if aim_ray.get_collider().is_in_group("target"):
-				aim_ray.get_collider().hit()
 				hits = hits + 1
 				score += 10
-				print("bazinga!!!")
 			elif  aim_ray.get_collider().is_in_group("head"):
-				aim_ray.get_collider().headshot()
 				hits = hits + 1 
 				score += 30
